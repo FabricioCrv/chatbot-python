1b772c942eaf8b11e97d4cc3ed83825e6a8a792c
FabricioCrv <fabriciogarciacarvalho87@hotmail.com>
2026-07-14 11:51:52 -0300

    adicionando arquivos

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from openai import OpenAI
+
+modelo_ia = OpenAI(api_key= "INSIRA_SUA_CHAVE_DE_API_AQUI")
+
+st.write("# Chatbot com IA do Fabrício")
+
+if not "lista_mensagens" in st.session_state:
+    st.session_state["lista_mensagens"] = []
+
+texto_usuario = st.chat_input("Digite sua mensagem")
+
+for mensagem in st.session_state["lista_mensagens"]:
+    role = mensagem["role"]
+    content = mensagem["content"]
+    st.chat_message(role).write(content)
+
+
+if texto_usuario:
+    print(texto_usuario)
+    st.chat_message('user').write(texto_usuario)
+    mensagem_usuario = {"role": "user", "content" : texto_usuario}
+    st.session_state["lista_mensagens"].append(mensagem_usuario)
+
+    resposta_ia = modelo_ia.chat.completions.create(messages= st.session_state["lista_mensagens"], model="gpt-4o")
+
+    texto_resposta_ia = resposta_ia.choices[0].message.content
+
+    mensagem_ia = {"role": "assistant", "content" : texto_resposta_ia}
+    st.chat_message('assistant').write(texto_resposta_ia)
+    st.session_state["lista_mensagens"].append(mensagem_ia)
